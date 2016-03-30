8d62f97e52e16b3f8c24f8ae9428344d592ddfbd
Nguyen Bao Xuan Truong <omega220@gmail.com>
2016-03-30 23:20:45 +0700

    Frist upload

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void m(Object o) {
+		System.out.println(o.getClass());		
+		System.out.println(o);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 0.123345667;
+		m(x);
+	}
+
+}
